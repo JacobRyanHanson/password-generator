5af40fefbb640e01c08fadaa0acd2db4f44d9dbb
JacobRyanHanson <jacobryanhanson@gmail.com>
2021-09-14 14:02:26 -0500

    Add style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,142 +1,133 @@
-// Get references to the #generate element
+const USER_CRITERIA = 4;
+
+// Gets references to the #generate element.
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
+// Event listner added to generateBtn.
 generateBtn.addEventListener("click", writePassword);
 
-// Write password to the #password input
+// Writes a password to the screen.
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  passwordText.value = password;
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
 }
 
-//generates password
 function generatePassword() {
+    var userPassLength = validNum();
+    var hasLowerCase = confirm("Include lowercase characters?");
+    var hasUpperCase = confirm("Include uppercase characters?");
+    var hasNumbers = confirm("Include numbers?");
+    var hasSpecial = confirm("Include special characters? e.g. !, \", #");
 
-  //user prompts
-  var userPassLength = validNum();
-  var hasLowerCase = confirm("Include lowercase characters?");
-  var hasUpperCase = confirm("Include uppercase characters?");
-  var hasNumbers = confirm("Include numbers?");
-  var hasSpecial = confirm("Include special characters? e.g. !, \", #");
+    var charTypes = [hasLowerCase, hasUpperCase, hasNumbers, hasSpecial];
+    var charIndices = [];
+    var array = [];
 
-  //arrays
-  var charTypes = [hasLowerCase, hasUpperCase, hasNumbers, hasSpecial];
-  var charIndices = [];
-  var array = [];
+    var password = "";
 
-  //local variables
-  var password = "";
-
-  //assigns the index for true values in charTypes to the first avaiable spot in charIndices
-  for (var i = 0; i < charTypes.length; i++) {
-    if (charTypes[i] === true) {
-      charIndices[charIndices.length] = i;
+    // Assigns the first avaiable index in charIndices for every true value in charTypes.
+    for (var i = 0; i < charTypes.length; i++) {
+        if (charTypes[i] === true) {
+            charIndices[charIndices.length] = i;
+        }
     }
-  }
 
-  //loops through each character in the password based on the users designated length
-  for (var i = 0; i < userPassLength; i++) {
+    // Loops through each character in the password based on the users designated length.
+    for (var i = 0; i < userPassLength; i++) {
 
-    //charIndices will have one index filled for each character type the user chose to include
-    //if its empty the loop breaks and an empty string is returned (user can press the button again)
-    if (charIndices.length === 0) {
-      alert("Please choose at least one character type.");
-      break;
-    } 
+        // If charIndices is empty the user didn't choose a character type and will be forced out.
+        // The user will need to press the button again to continue.
+        if (charIndices.length === 0) {
+            alert("Please choose at least one character type.");
+            break;
+        }
 
-    //before the last three characters the characters are generated randomly with charGen()
-    else if (password.length < userPassLength - 3) {
+        // Generates password randomly until the last three characters
+        else if (password.length < userPassLength - 3) {
+            password = charGen(charIndices, password);
+        }
 
-      //password is passed in and returned with an additional character
-      password = charGen(charIndices, password);
-    } 
-    
-    //uses regex() and subsquent method (explained below) to return a value if the password is almost complete
-    //but a user character selection has not yet been randomly generated, generating the character randomly if not
-    //or generating characters regularly if user selections have been satisfied
-    else {
-      var index = regex(charIndices, password)
-      if (index === 0 || index === 1 || index === 2 || index === 3) {
-        array = [index];
-        password = charGen(array, password);
-      } else {
-        password = charGen(charIndices, password);
-      }
+        // Checks if a character is missing based on the user's selection so  a character type incursion can be made if necessary.
+        else {
+            var index = regex(charIndices, password)
+
+            // Indicates that user character type is missing and should be included.
+            if (index === 0 || index === 1 || index === 2 || index === 3) {
+                array = [index];
+                password = charGen(array, password);
+            } else {
+                password = charGen(charIndices, password);
+            }
+        }
     }
-  }
-  return password;
+    return password;
 }
 
-//validates user input for password length
+// Validates user input for password length.
 function validNum() {
-  do {
-    var valid = true;
-    var response = parseInt(prompt("Please enter password length."));
-    if (response >= 8 && response <= 128 && response) {
-      return response;
-    } else {
-      alert("Invalid input. Please enter a number between 8 and 128.");
-      valid = false;
-    }
-  } while (!valid)
+    do {
+        var valid = true;
+        var response = parseInt(prompt("Please enter password length."));
+        if (response >= 8 && response <= 128 && response) {
+            return response;
+        } else {
+            alert("Invalid input. Please enter a number between 8 and 128.");
+            valid = false;
+        }
+    } while (!valid)
 }
 
-//generates a number randomly given a range
+// Generates a number randomly, given a range.
 function randNum(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
+    return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-//searches the current password for each regex expression in the array
-//if the expression (user character selection) is not found and shouldExist() is true
-//it returns the index which is handeled by the last section in generatePassword() to 
-//generate the appropriate character
+// Searches the password generated thus far as detailed by the regular expressions in the array.
+// If the regular expressions didn't find anything and the expression should exist (detailed below)
+// i is returned indicating the index in charIndices() (the character type) that should be included.
 function regex(charIndices, password) {
-  var pattern = [/[a-z]/, /[A-Z]/, /[0-9]/, /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/];
-  for (var i = 0; i < 4; i++) {
-    if (pattern[i].test(password) === false && shouldExist(charIndices, i)) {
-      return i;
+    var pattern = [/[a-z]/, /[A-Z]/, /[0-9]/, /[~`!@#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?()\._]/];
+    for (var i = 0; i < USER_CRITERIA; i++) {
+        if (pattern[i].test(password) === false && shouldExist(charIndices, i)) {
+            return i;
+        }
     }
-  }
 }
 
-
-//matches the current loop cound (i) in regex() aginst the possible values charIndices[] can hold
-//to check if that user character selection should exist (but currently dosent)
+// Checks each index of charIndices for index (the character type) and if it exists returns true, else returns false.
 function shouldExist(charIndices, index) {
-  for (var i = 0; i < charIndices.length; i++) {
-    if (charIndices[i] === index) {
-      return true;
-    } 
-  }
-  return false;
+    for (var i = 0; i < charIndices.length; i++) {
+        if (charIndices[i] === index) {
+            return true;
+        }
+    }
+    return false;
 }
 
-//generates characters for password based on user choices by randomly selecting and evaluating one of 
-//charIndices[] indices (length may vary but the numerical values held are representitive)
-//selected indicies are converted to characters and returned
+// Generates characters for password based on user choices by randomly selecting and evaluating one of 
+// charIndices[] indices. Selected indicies are converted to characters, concatenated, and returned.
 function charGen(charIndices, password) {
-  var index = randNum(0, charIndices.length - 1);
+    var index = randNum(0, charIndices.length - 1);
 
-  //randNum ranges based on HTML ASCII Character Set
-  if (charIndices[index] === 0) {
-    password += String.fromCharCode(randNum(97, 122));
-  } else if (charIndices[index] === 1) {
-    password += String.fromCharCode(randNum(65, 90));
-  } else if (charIndices[index] === 2) {
-    password += String.fromCharCode(randNum(48, 57));
-  } else {
-    var specialChar = randNum(1, 32);
-    if (specialChar <= 15) { //32-47
-      password += String.fromCharCode(specialChar + 32);
-    } else if (specialChar <= 22) { //58-64
-      password += String.fromCharCode(specialChar + 42);
-    } else if (specialChar <= 28) { //91-96
-      password += String.fromCharCode(specialChar + 68);
-    } else { //123-126
-      password += String.fromCharCode(specialChar + 94);
+    // Ranges based on HTML ASCII Character Set
+    if (charIndices[index] === 0) {
+        password += String.fromCharCode(randNum(97, 122));
+    } else if (charIndices[index] === 1) {
+        password += String.fromCharCode(randNum(65, 90));
+    } else if (charIndices[index] === 2) {
+        password += String.fromCharCode(randNum(48, 57));
+    } else {
+        var specialChar = randNum(1, 32);
+        if (specialChar <= 15) { // 32-47
+            password += String.fromCharCode(specialChar + 32);
+        } else if (specialChar <= 22) { // 58-64
+            password += String.fromCharCode(specialChar + 42);
+        } else if (specialChar <= 28) { // 91-96
+            password += String.fromCharCode(specialChar + 68);
+        } else { // 123-126
+            password += String.fromCharCode(specialChar + 94);
+        }
     }
-  }
-  return password;
+    return password;
 }
